9af652dccb55675692c66ecc3deddd00878eec47
DenisDragos1 <dragosdenisdanut@yahoo.com>
2024-03-05 15:00:52 +0200

    navbar+footer+button

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
 import '../styles/globals.css';
 
+import { NavBar } from '../components/componentsindex';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+<div>
+  <NavBar/>
+ <Component {...pageProps} />;
+ </div>
 }
